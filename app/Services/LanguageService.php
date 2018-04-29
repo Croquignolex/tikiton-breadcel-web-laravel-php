f9f0edf2132c2966e088018503a6b68ee37419fa
Alex Stéphane <gpetitalex10@gmail.com>
2018-04-29 07:15:49 +0100

    Add language dictionaries

---
+++ app/Services/LanguageService.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\App;
+
+class LanguageService
+{
+    /**
+     * @return array
+     */
+    public function getLanguages()
+    {
+        return config('app.locales');
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrentLanguage()
+    {
+        return App::getLocale();
+    }
+
+    /**
+     * @param $language
+     * @return mixed|string
+     */
+    public function getUrl($language)
+    {
+        $fullUrl = url()->full();
+
+        if(in_array($language, $this->getLanguages()))
+        {
+            foreach ($this->getLanguages() as $lang)
+            {
+                if(str_contains($fullUrl, $lang))
+                    return str_replace($lang, $language, $fullUrl);
+            }
+        }
+
+        $url = config('app.url');
+        return str_replace_first($url, $url . '/' . $language, $fullUrl);
+    }
+
+    /**
+     * @param $language
+     * @return string
+     */
+    public function getLanguageFullName($language)
+    {
+        if(in_array($language, $this->getLanguages())) return trans('language.' . $language);
+        else return 'language.unknown';
+    }
+
+    /**
+     * @param $language
+     * @return bool
+     */
+    public function isActiveLanguage($language)
+    {
+        return $this->getCurrentLanguage() == $language;
+    }
+}
---
+++ resources/lang/fr/validation.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'required'             => 'Ce champ est réquis.',
+    'string'               => 'Ce champ doit être une chaîne de charactères.',
+    'email'                => 'Ce champ doit être une addresse mail valide.',
+    'unique'               => 'Ce champ doit être unique.',
+    'confirmed'            => 'Ce champ doit être confimé.',
+    'max'                  => [
+        'numeric' => 'Ce champ doit contenir au plus une valeur de :max.',
+        'string'  => 'Ce champ doit contenir :max charactères au plus.'
+    ],
+    'min'                  => [
+        'numeric' => 'Ce champ doit contenir au moins une valeur de :min.',
+        'string'  => 'Ce champ doit contenir :min charactères au moins.'
+    ]
+];
